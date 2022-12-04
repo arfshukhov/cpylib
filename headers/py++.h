1be23edb04b01d512c11c18f9a2101f82d53e1bd
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2022-12-05 01:51:24 +0500

    Add files via upload

---
+++ headers/py++.h
@@ -0,0 +1,24 @@
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+#ifndef _PYPP_H
+#define	_PYPP_H
+
+
+
+string get_str(string, int);
+
+string join(vector<string>, string);
+
+void rstrip(string&, char);
+
+void lstrip(string&, char);
+
+#endif 
