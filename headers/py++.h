242b11dd44ebdd46d7f8d04b67df076520792b1e
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2022-12-06 01:32:25 +0500

    Add files via upload

--- headers/py++.h
+++ headers/py++.h
@@ -8,12 +8,16 @@ using namespace std;
 #include <list>
 #include <algorithm>
 
+
+
 #ifndef _PYPP_H
 #define	_PYPP_H
 
+string readline(string, int);
 
+vector<string> readlines(string path);
 
-string get_str(string, int);
+string read(string path);
 
 string join(vector<string>, string);
 
@@ -21,4 +25,11 @@ void rstrip(string&, char);
 
 void lstrip(string&, char);
 
+template <template <typename, typename> typename Container, typename T, typename Alloc = allocator<T>>
+void print(Container<T, Alloc> vec, string separator = " ") {
+    for (auto i = 0; i < vec.size()-1; i++) {
+        cout << vec[i] << separator;
+    }
+    cout << vec[vec.size() - 1];
+}
 #endif 
